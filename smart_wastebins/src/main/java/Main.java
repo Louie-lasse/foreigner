fa61261f81b9ec716f17ca63390e6e6cfcfd91b4
rhedinh <rhedinh@student.chalmers.se>
2022-04-06 15:48:40 +0200

    Create Main.java
    The program prints fullness of all wastebins

--- smart_wastebins/src/main/java/Main.java
+++ smart_wastebins/src/main/java/Main.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Wastebin bin1 = new Wastebin(57, 12, 0.5);
+        Wastebin bin2 = new Wastebin(57, 12, 0.8);
+        Wastebin bin3 = new Wastebin(57, 12, 0.07);
+        List<Wastebin> wastebins = new ArrayList<>();
+        wastebins.add(bin1);
+        wastebins.add(bin2);
+        wastebins.add(bin3);
+
+        for (int i = 0; i < wastebins.size(); i++) {
+            System.out.println(wastebins.get(i).getFullness());
+        }
+    }
+
 }
