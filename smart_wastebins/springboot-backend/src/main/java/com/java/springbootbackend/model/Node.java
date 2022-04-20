15ce84ee00dbebaa33244e2ff22067c26993d202
arvidsve <arvidsve@chalmers.se>
2022-04-20 16:30:25 +0200

    Creates class Node

---
+++ smart_wastebins/springboot-backend/src/main/java/com/java/springbootbackend/model/Node.java
@@ -0,0 +1,14 @@
+package com.java.springbootbackend.model;
+
+public class Node {
+
+    Coord coord;
+
+    public Node(Coord coord) {
+        this.coord = coord;
+    }
+
+    public Coord getCoord() {
+        return coord;
+    }
+}
