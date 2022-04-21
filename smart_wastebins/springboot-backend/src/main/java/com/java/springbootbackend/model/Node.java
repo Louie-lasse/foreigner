08f641cd6c7af5fe74f813e5fd29bb83da8583ea
arvidsve <arvidsve@chalmers.se>
2022-04-21 16:24:56 +0200

    Adds classes Node and Edge.

--- smart_wastebins/springboot-backend/src/main/java/com/java/springbootbackend/model/Node.java
+++ smart_wastebins/springboot-backend/src/main/java/com/java/springbootbackend/model/Node.java
@@ -1,14 +1,23 @@
 package com.java.springbootbackend.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Node {
 
     Coord coord;
+    List<Edge> edges;
 
     public Node(Coord coord) {
         this.coord = coord;
+        this.edges = new ArrayList<>();
     }
 
     public Coord getCoord() {
         return coord;
+    }
+
+    public void addEdge(Coord c, double w) {
+        edges.add(new Edge(c,w));
     }
 }
