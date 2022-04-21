845697b314c08c1452dd335059209272a33c057a
Louie-lasse <love.lindqvist101@gmail.com>
2022-04-21 16:48:47 +0200

    Adds Graph.java

---
+++ smart_wastebins/springboot-backend/src/main/java/com/java/springbootbackend/model/Graph.java
@@ -0,0 +1,34 @@
+package com.java.springbootbackend.model;
+
+import com.java.springbootbackend.services.Weight.WeightService;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Graph {
+
+    private final Map<Coord, Node> nodes;
+
+    public Graph(List<Coord> coords){
+        WeightService weightService = WeightService.getService();
+        nodes = new HashMap<>();
+        Node node;
+        double weight;
+        for (Coord c : coords){
+            node = new Node(c);
+            for (Coord other : coords) {
+                if (!c.equals(other)){
+                    weight = weightService.calculateWeight(c, other);
+                    node.addEdge(other, weight);
+                }
+            }
+            nodes.put(c, node);
+        }
+    }
+
+    public List<Coord> shortestPath(){
+        throw new RuntimeException("Not implemented yet :.(");
+    }
+
+}
