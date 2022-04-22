70b1bafab98d5526036ec6ac88e17ec418da9461
Louie-lasse <love.lindqvist101@gmail.com>
2022-04-22 11:35:58 +0200

    Adds infrastructure for PathFinderService

---
+++ smart_wastebins/springboot-backend/src/main/java/com/java/springbootbackend/services/PathFinder/BasicPathFinder.java
@@ -0,0 +1,16 @@
+package com.java.springbootbackend.services.PathFinder;
+
+import com.java.springbootbackend.model.Coord;
+import com.java.springbootbackend.model.Node;
+
+import java.util.List;
+import java.util.Map;
+
+public class BasicPathFinder extends PathFinderService {
+
+
+    @Override
+    protected List<Coord> calculatePath(Map<? extends Coord, ? extends Node> map) {
+        throw new RuntimeException("Not implemented yet");
+    }
+}
---
+++ smart_wastebins/springboot-backend/src/main/java/com/java/springbootbackend/services/PathFinder/PathFinderService.java
@@ -0,0 +1,27 @@
+package com.java.springbootbackend.services.PathFinder;
+
+import com.java.springbootbackend.model.Coord;
+import com.java.springbootbackend.model.Node;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public abstract class PathFinderService {
+
+    private static PathFinderService implementation;
+
+    public static PathFinderService getImplementation() {
+        if (Objects.isNull(implementation)) {
+            implementation = new BasicPathFinder();
+        }
+        return implementation;
+    }
+
+    public List<Coord> findPath(Map<? extends Coord, ? extends Node> map) {
+        return getImplementation().calculatePath(map);
+    }
+
+    protected abstract List<Coord> calculatePath(Map<? extends Coord, ? extends Node> map);
+
+}
