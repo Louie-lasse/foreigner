4296559d448fa93d4dfce64a6f9865557fd6be58
rhedinh <rhedinh@student.chalmers.se>
2022-04-29 14:28:28 +0200

    A input in frontend prints out input in console

--- smart_wastebins/react-hooks-frontend/src/App.js
+++ smart_wastebins/react-hooks-frontend/src/App.js
@@ -2,9 +2,29 @@ import './App.css';
 import ListBinComponent from './components/ListBinComponent';
 
 function App() {
+const handleSubmit=(e)=>{
+  e.preventDefault();
+  const nBins = e.target.numberOfBins.value;
+  console.log("Number of bins: " + nBins);
+}
+
   return (
     <div>
       <ListBinComponent />
+      <form onSubmit={handleSubmit}>
+        <input
+          onKeyPress={(event) => {
+            if (!/[0-9]/.test(event.key)) {
+              event.preventDefault();
+            }
+          }}
+          name = "numberOfBins"
+          placeholder="Number Of Bins"
+        />
+        <button>
+          Collect wastebins
+        </button>
+      </form>
     </div>
   );
 }
--- smart_wastebins/react-hooks-frontend/src/services/BinService.js
+++ smart_wastebins/react-hooks-frontend/src/services/BinService.js
@@ -1,11 +1,15 @@
 import axios from 'axios'
 
 const BIN_BASE_REST_API_URL = 'http://localhost:8081/api/v1/wastebins';
+const BIN_BASE_REST_API_URL1 = 'http://localhost:8081/api/v1/wastebins/5';
 
 class BinService {
-
     getAllBins() {
         return axios.get(BIN_BASE_REST_API_URL)
+    }
+
+    getBins() {
+        return axios.get(BIN_BASE_REST_API_URL1)
     }
 }
 
