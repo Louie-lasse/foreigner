128439669f35d6b1250d509880e11e3d547933f4
Louie-lasse <love.lindqvist101@gmail.com>
2022-05-04 16:01:29 +0200

    Makes map display path between pins

--- smart_wastebins/react-hooks-frontend/src/components/MapComponent.js
+++ smart_wastebins/react-hooks-frontend/src/components/MapComponent.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker, Polyline } from '@react-google-maps/api';
 
 const containerStyle = {
   width: '1280px',
@@ -23,6 +23,28 @@ function getMarkers(coordinates) {
   return markers;
 }
 
+function generatePath(coordinates){
+  let coords = coordinates.coordinates
+  if (coords.length === 0){
+    console.log('empty')
+    return <></>
+  } 
+  console.log(coords)
+  let paths = []
+  let i = 0;
+  while (i < coords.length){
+    let coord = coords[i];
+    paths[i] = {lat: coord.x, lng: coord.y}
+    i++;
+  }
+  let coord = coords[0];
+  paths[i] = {lat: coord.x, lng: coord.y}
+  console.log(paths)
+  return <Polyline
+          path={paths}
+          />;
+}
+
 function MapComponent(coordinates) {
   return (
     <div>
@@ -35,6 +57,7 @@ function MapComponent(coordinates) {
         zoom={12}
       >
         {getMarkers(coordinates)}
+        {generatePath(coordinates)}
       </GoogleMap>
     </LoadScript>
     </div>
