24c3314d43623c87145a71711a46d52765a120c9
RevolverGoesPang <juliagiaro@hotmail.com>
2022-05-11 19:42:48 +0200

    fixed errors and bugs

--- smart_wastebins/react-hooks-frontend/src/App.js
+++ smart_wastebins/react-hooks-frontend/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState,useEffect } from 'react';
 import './App.css';
 import CollectBins from './components/CollectBins';
--- smart_wastebins/react-hooks-frontend/src/components/InputPath.js
+++ smart_wastebins/react-hooks-frontend/src/components/InputPath.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React from 'react';
 import BinService from "../services/BinService";
 import InputBin from "./InputBin";
 
