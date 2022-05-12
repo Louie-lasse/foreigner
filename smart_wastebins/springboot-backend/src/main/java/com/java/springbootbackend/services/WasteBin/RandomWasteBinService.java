ab59a55eaab3675563dbdc826c69b8fbf220d554
arvidsve <arvidsve@chalmers.se>
2022-05-12 17:34:12 +0200

    Fixes RandomWasteBinService class.
    Co-authored-by: rhedinh <rhedinh@student.chalmers.se>
    Co-authored-by: Louie-lasse <love.lindqvist101@gmail.com>

--- smart_wastebins/springboot-backend/src/main/java/com/java/springbootbackend/services/WasteBin/RandomWasteBinService.java
+++ smart_wastebins/springboot-backend/src/main/java/com/java/springbootbackend/services/WasteBin/RandomWasteBinService.java
@@ -22,17 +22,35 @@ class RandomWasteBinService implements IWasteBinService {
         return rangeMin + (rangeMax - rangeMin) * random.nextDouble();
     }
 
+    private int randomInt(int rangeMin, int rangeMax) {
+        return (int) Math.floor(rangeMin + (rangeMax - rangeMin) * random.nextDouble());
+    }
+
     @Override
     public List<WasteBin> getWasteBins() {
         if (Objects.isNull(wasteBins)) {
             random = new Random(seed);
             wasteBins = new ArrayList<>();
             for (int i = 0; i < 15; i++) {
-                double x, y, fullness;
+                double x, y;
+                int fullness;
+                long serialNumber;
+                String groupName, description;
+                String[] groups = {"Väst", "Öst", "Nord"};
+                String alphabet = "abcdefghijklmnopqrstuvwxyz";
+                StringBuilder sb = new StringBuilder();
                 x = randomDouble(57.634763, 57.761942);
                 y = randomDouble(11.889930, 12.053338);
-                fullness = randomDouble(0.4, 1);
-                wasteBins.add(new WasteBin(x, y, fullness));
+                fullness = randomInt(0,10);
+                groupName = groups[randomInt(0,3)];
+                serialNumber = randomInt(0, 20000000);
+                for (int j = 0; j < 15; j++) {
+                    int rand = randomInt(0,25);
+                    sb.append(alphabet.charAt(rand));
+                }
+                description = sb.toString();
+
+                wasteBins.add(new WasteBin(x, y, fullness, groupName, serialNumber, description));
             }
         }
         return wasteBins;
